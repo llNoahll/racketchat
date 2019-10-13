2c74f20b31993eda6a5c30d35fb11cc277d7141d
Noah <537520539@qq.com>
2019-10-13 19:11:37 +0800

    Switch to typed/racket version.

--- client.rkt
+++ client.rkt
@@ -1,28 +1,46 @@
-#lang racket
+#lang typed/racket
 
-(require racket/tcp racket/gui racket/draw)
+(require racket/tcp typed/racket/gui typed/racket/draw)
 
 
-(define SERVER-PORT 1234)
 (define SERVER-IP "127.0.0.1")
+(define SERVER-PORT 1234)
+
 
 (define login-dialog (instantiate dialog% ("RacketChat")))
-(define login-panel (new horizontal-panel% [parent login-dialog] [alignment '(center center)]))
-(define username-field (new text-field%  [parent login-dialog] [label "login or choose a username"]))
+(define login-panel
+  (new horizontal-panel%
+       [parent login-dialog]
+       [alignment '(center center)]))
+(define username-field
+  (new text-field%
+       [parent login-dialog]
+       [label "login or choose a username"]))
 (define window (new frame% [label "RacketChat"]))
-(define editor-canvas (new editor-canvas% (parent window) (label "Chat") [min-height 300] [min-width 600]))
+(define editor-canvas
+  (new editor-canvas%
+       [parent window]
+       [label "Chat"]
+       [min-height 300]
+       [min-width 600]))
 (define text (new text%))
 (define hpanel (new horizontal-panel% [parent window]))
 (define input-field (new text-field% [parent hpanel] [label "Input"]))
 
+
+(: exn-handler [-> exn:fail:network Void])
 (define exn-handler
   (λ (exception)
     (send text insert "Error sending data to server\n")))
 
+
+(: setup-send-button [-> Input-Port Output-Port String Void])
 (define setup-send-button
   (λ (in out username)
+    (: send-btn-callback [-> (Instance Button%) (Instance Control-Event%)
+                             (U Thread Void)])
     (define send-btn-callback
-      (λ (button exception)
+      (λ (button event)
         (with-handlers ([exn:fail:network? exn-handler])
           (thread
            (λ ()
@@ -42,15 +60,20 @@
 
     (void)))
 
+
+(: handle-bad-username [-> (U Input-Port Null) (U Output-Port Null) Any Void])
 (define handle-bad-username
   (λ (in out username)
+    ;; TODO: maybe we'll use the username in the future.
     (send username-field set-value "")
     (send username-field set-label "Username invalid")
 
-    (unless (and (null? in) (null? out))
-      (close-output-port out)
+    (unless (null? out)
+      (close-output-port out))
+    (unless (null? in)
       (close-input-port in))))
 
+(: handle-post-login [-> Input-Port Output-Port String (U Thread Void)])
 (define handle-post-login
   (λ (in out username)
     (setup-send-button in out username)
@@ -64,12 +87,14 @@
         (let msg-checker ([port (sync/timeout 0 in)])
           (unless (eq? #f port)
             (define msg (read-line port))
+
             (unless (eof-object? msg)
               (send text insert
                     (string-append msg "\n"))))
           (msg-checker (sync/timeout 0 in))))))))
 
 
+(: login [-> String String Natural (U Thread Void)])
 (define login
   (λ (username ip port)
     (define login-cust (make-custodian))
@@ -82,16 +107,23 @@
         (flush-output out)
         (thread
          (λ ()
-           (if (zero? (read-byte in))
-               (handle-bad-username in out username)
-               (handle-post-login   in out username))))))))
+           (let ([flag (read-byte in)])
+             (if (or (eof-object? flag)
+                     (zero? flag))
+                 (handle-bad-username in out username)
+                 (handle-post-login   in out username)))))))))
 
+
+(: login-callback [-> (Instance Button%) (Instance Event%) (U Thread Void)])
 (define login-callback
   (λ (button event)
+    (: valid-username? [-> Any Boolean : #:+ (and String (! ""))])
     (define valid-username?
       (λ (name)
+        (define-predicate empty-string? "")
+
         (and (string? name)
-             (not (zero? (string-length name))))))
+             (not (empty-string? name)))))
 
     (define username (send username-field get-value))
 
@@ -100,13 +132,17 @@
         (login username SERVER-IP SERVER-PORT)
         (handle-bad-username '() '() username))))
 
+(: cancel-callback [-> (Instance Button%) (Instance Event%) Nothing])
 (define cancel-callback
   (λ (button event)
     (exit)))
 
-(new button% [parent login-panel] [label "Cancel"] [callback cancel-callback])
-(new button% [parent login-panel] [label "Login" ] [callback login-callback])
 
+(new button% [parent login-panel] [label "Login" ] [callback login-callback])
+(new button% [parent login-panel] [label "Cancel"] [callback cancel-callback])
+
+
+(: start [-> Void])
 (define start
   (λ ()
     (send editor-canvas enable #f)
--- server.rkt
+++ server.rkt
@@ -1,15 +1,31 @@
-#lang racket
+#lang typed/racket
 
 (require racket/tcp)
 
 
 (define PORT 1234)
 
-(define connections '())
-(define (add-connection! conn) (set! connections (append connections (list conn))))
-(define (rem-connection! conn) (set! connections (filter (λ (x) (not (eq? x conn))) connections)))
 
-(define-struct connection (in out username))
+(define-struct connection
+  ([in : Input-Port]
+   [out : Output-Port]
+   [username : String]))
+
+(: connections (Listof connection))
+(define connections '())
+
+(: add-connection! [-> connection Void])
+(define add-connection!
+  (λ (conn)
+    (set! connections (append connections (list conn)))))
+
+(: rem-connection! [-> connection Void])
+(define rem-connection!
+  (λ (conn)
+    (set! connections (filter (λ ([x : connection]) (not (eq? x conn)))
+                              connections))))
+
+(: connection-close! [-> connection Thread])
 (define connection-close!
   (λ (conn)
     (close-input-port  (connection-in conn))
@@ -17,37 +33,48 @@
     (rem-connection! conn)
     (broadcast* (string-append (connection-username conn) " has left."))))
 
+
+(: broadcast [-> String connection Thread])
 (define broadcast
   (λ (msg sender-conn)
+    (: send-to [-> connection (U Thread Void)])
     (define send-to
       (λ (conn)
+        (: exn-handler [-> exn:fail:network Thread])
         (define exn-handler
           (λ (exception)
             (displayln "network error")
             (displayln exception)
             (rem-connection! conn)
-            (broadcast* (string-append (connection-username conn) " has left."))))
+            (broadcast* (string-append (connection-username conn)
+                                       " has left."))))
 
         (with-handlers ([exn:fail:network? exn-handler])
-          (displayln (string-append (connection-username sender-conn) ": " msg) (connection-out conn))
+          (displayln (string-append (connection-username sender-conn)
+                                    ": " msg)
+                     (connection-out conn))
           (flush-output (connection-out conn)))))
 
     (thread
      (λ () (map send-to
-                (filter (λ (conn)
+                (filter (λ ([conn : connection])
                           (not (eq? conn sender-conn)))
                         connections))))))
 
+(: broadcast* [->* (String) ((U connection Null)) Thread])
 (define broadcast*
   (λ (msg (sender-conn '()))
+    (: send-to [-> connection (U Thread Void)])
     (define send-to
       (λ (conn)
+        (: exn-handler [-> exn:fail:network Thread])
         (define exn-handler
           (λ (exception)
             (displayln "network error")
             (displayln exception)
             (rem-connection! conn)
-            (broadcast* (string-append (connection-username conn) " has left."))))
+            (broadcast* (string-append (connection-username conn)
+                                       " has left."))))
 
         (with-handlers ([exn:fail:network? exn-handler])
           (displayln msg (connection-out conn))
@@ -56,18 +83,22 @@
     (if (null? sender-conn)
         (thread (λ () (map send-to connections)))
         (thread (λ () (map send-to
-                           (filter (λ (conn)
+                           (filter (λ ([conn : connection])
                                      (not (eq? conn sender-conn)))
                                    connections)))))))
 
+
+(: handle [-> connection (U Thread Void)])
 (define handle
   (λ (connection)
+    (: exn-handler [-> exn:fail:network Thread])
     (define exn-handler
       (λ (exception)
         (displayln "network error")
         (displayln exception)
         (rem-connection! connection)
-        (broadcast* (string-append (connection-username connection) " has left."))))
+        (broadcast* (string-append (connection-username connection)
+                                   " has left."))))
 
     (with-handlers ([exn:fail:network? exn-handler])
       (define msg (read-line (connection-in connection)))
@@ -76,61 +107,75 @@
         (broadcast msg connection)
         (handle connection)))))
 
+(: get-username [-> Input-Port Output-Port String])
 (define get-username
   (λ (in out)
+    (: valid-username? [-> Any Boolean : #:+ (and String (! ""))])
     (define valid-username?
-      (λ (name)
+      (lambda (name)
+        (define-predicate empty-string? "")
+
         (and (string? name)
-             (not (zero? (string-length name))))))
+             (not (empty-string? name)))))
 
     ;; TODO: actually do something useful here.
+    (: exn-handler [-> exn:fail:network String])
     (define exn-handler
       (λ (exception)
         (displayln "network error")
-        (displayln exception)))
+        (displayln exception)
+
+        ;; return a valid name ("").
+        ""))
 
     (with-handlers ([exn:fail:network? exn-handler])
       (define name (read-line in))
 
-      (cond [(not (valid-username? name))
-             (write-byte 0 out) (flush-output out) ; tell the client it's name is invalid.
-             (get-username in out)]
-            [else
+      (cond [(valid-username? name)
              (write-byte 1 out) (flush-output out) ; tell the client it's name is valid.
-             name]))))
+             name]
+            [else
+             (write-byte 0 out) (flush-output out) ; tell the client it's name is invalid.
+             (get-username in out)]))))
 
+(: accept-and-handle [-> TCP-Listener Thread])
 (define accept-and-handle
   (λ (listener)
     (define cust (make-custodian))
 
     (parameterize ([current-custodian cust])
       (define-values (in out) (tcp-accept listener))
+
       (define username (get-username in out))
 
-      (when (string? username)
-        (thread
-         (λ ()
-           (define conn (connection in out username))
+      (thread
+       (λ ()
+         (define conn (connection in out username))
 
-           (add-connection! conn)
-           (broadcast* (string-append username " has joined.") conn)
-           (handle conn)
-           (connection-close! conn)))))))
+         (add-connection! conn)
+         (broadcast* (string-append username " has joined.") conn)
+         (handle conn)
+         (connection-close! conn))))))
 
 
+(: serve [-> Natural [-> Void]])
 (define serve
   (λ (port)
     (define main-cust (make-custodian))
 
     (parameterize ([current-custodian main-cust])
       (define listener (tcp-listen port 5 #t))
-      (define (loop)
-        (accept-and-handle listener)
-        (loop))
+
+      (: loop [-> Void])
+      (define loop
+        (λ ()
+          (accept-and-handle listener)
+          (loop)))
 
       (thread loop))
 
     (λ () (custodian-shutdown-all main-cust))))
 
 
+(: stop [-> Void])
 (define stop (serve PORT))
